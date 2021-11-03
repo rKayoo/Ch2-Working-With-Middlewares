d6eef71d1c34ee0dc773c68f16d278c772096d17
Rafael Kayo <rafael.kayo99@gmail.com>
2021-11-03 13:11:30 -0400

    check if todo exists / find an user by its id

--- src/index.js
+++ src/index.js
@@ -45,20 +45,35 @@ function checksTodoExists(request, response, next) {
   const isUuid = validate(id);
 
   if(!user) {
-    return response.status(404).json({ error: "User not found"});
-  }  
-  
-  if(!isUuid) {
-    return response.status(400).send();
+    return response.status(404).json({ error: "User not found" });
+  } else if(!isUuid) {
+    return response.status(400);
   } 
 
+  const todo = user.todos.find(todo => todo.id === id);
+  
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found"});
+  }
+
   request.user = user;
+  request.todo = todo;
 
   return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
